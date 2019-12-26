c76cc1a79dcddaccd25452d095e155262a878993
Stijn Goethals <stijn_goethals@hotmail.com>
2019-12-26 13:41:16 +0100

    update code

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fuzzywuzzy import fuzz
 import jinja2
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+from time import sleep
 
 FILE = "data.csv"
 BUDDIES = []
@@ -14,9 +15,15 @@ BUDDIES = []
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 TEMPLATE_FILE = "template.txt"
+FEEDBACK_FILE = "template-feedback.txt"
+CONFIRM_FILE = "template-confirm.txt"
 template = templateEnv.get_template(TEMPLATE_FILE)
+feedback = templateEnv.get_template(FEEDBACK_FILE)
+confirm = templateEnv.get_template(CONFIRM_FILE)
 
-URL = 'http://192.168.0.172:8080/v1/sms/' # Set destination URL here
+URL = 'http://192.168.1.3:8080/v1/sms/' # Set destination URL here
+
+random.seed(4)
 
 # Fields this year:
 ###########
@@ -167,8 +174,8 @@ def printStatistics():
     print("Aantal niet middelbaar:", notmiddelbaar)
     print("Alle districten (fuzzy matching):", ', '.join(districten))
     print("")
-
-def sendMessage(user, match):
+    
+def send_match_message(user, match):
     phone = user.props['Gsmnummer'].strip()
         
     message = template.render(user=user.props, match=match.props)
@@ -178,13 +185,81 @@ def sendMessage(user, match):
     json = urlopen(request).read().decode()
     print(json)
     print(message)
+    
+    
+def send_feedback_message(user):
+    phone = user.props['Gsmnummer'].strip()
+        
+    message = feedback.render(user=user.props)
+    post_fields = {'message': message, "phone": phone}     # Set POST fields here
 
-def main():
+    request = Request(URL, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
+    print(json)
+    print(message) 
+    
+def send_confirm_message(user):
+    phone = user.props['Gsmnummer'].strip()
+        
+    message = confirm.render(user=user.props)
+    post_fields = {'message': message, "phone": phone}     # Set POST fields here
+
+    request = Request(URL, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
+    print(json)
+    print(message)
+
+def send_feedback():
+    persons = []
     with open(FILE, 'r', encoding="utf-8") as csvFile:
         buddyReader = csv.DictReader(csvFile, delimiter=',', quotechar='"')
         for row in buddyReader:
-            if(row["Schooltype"] != "Middelbaar"):
+            if(row["Schooltype"] == "Middelbaar"):
                 continue
+            newbuddy = Buddy(row)
+
+            # Make sure no double phone numbers enter
+            for buddy in persons:
+                if buddy.props["Gsmnummer"] == newbuddy.props["Gsmnummer"]:
+                    print("Dubbele entry gevonden:", buddy)
+                    persons.remove(buddy)
+                    break
+
+            persons.append(newbuddy)
+
+    for buddy in persons:
+        send_feedback_message(buddy)
+        sleep(60)
+
+def send_confirm():
+    persons = []
+    with open(FILE, 'r', encoding="utf-8") as csvFile:
+        buddyReader = csv.DictReader(csvFile, delimiter=',', quotechar='"')
+        for row in buddyReader:
+            if(row["Schooltype"] == "Middelbaar"):
+                continue
+            newbuddy = Buddy(row)
+
+            # Make sure no double phone numbers enter
+            for buddy in persons:
+                if buddy.props["Gsmnummer"] == newbuddy.props["Gsmnummer"]:
+                    print("Dubbele entry gevonden:", buddy)
+                    persons.remove(buddy)
+                    break
+
+            persons.append(newbuddy)
+    
+    print(len(persons))
+    for buddy in persons:
+        send_confirm_message(buddy)
+        sleep(30)
+
+def send_match():
+    with open(FILE, 'r', encoding="utf-8") as csvFile:
+        buddyReader = csv.DictReader(csvFile, delimiter=',', quotechar='"')
+        for row in buddyReader:
+            # if(row["Schooltype"] == "Middelbaar"):
+                # continue
             newbuddy = Buddy(row)
 
             # Make sure no double phone numbers enter
@@ -200,10 +275,14 @@ def main():
 
     matches = matchmaker()
     for match in matches:
-        sendMessage(match[0], match[1])
-        sendMessage(match[1], match[0])
+        send_match_message(match[0], match[1])
+        sleep(60)
+        send_match_message(match[1], match[0])
+        sleep(60)
         print(match[0].props["Naam"][:20].ljust(22), match[0].props["District"][:14].ljust(16), match[0].props["Schooltype"][:18].ljust(20), end="")
         print(" - ", end="")
         print(match[1].props["Naam"][:20].ljust(22), match[1].props["District"][:14].ljust(16), match[1].props["Schooltype"][:18].ljust(20))
 if __name__ == "__main__":
-    main()
+    #send_feedback()
+    send_confirm()
+    #send_match()
